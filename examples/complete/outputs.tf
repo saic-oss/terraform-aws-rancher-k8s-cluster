0d7c897e0e6971f5df0947115ad315804a9f45fb
Andrew Roth <roth.andy@gmail.com>
2020-11-04 14:08:31 -0500

    Fix example, helm repo urls, readme, dep versions

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -4,13 +4,13 @@ output "master_ssh_public_key" {
 }
 
 output "master_ssh_private_key" {
-  description = "[SENSITIVE] Private SSH key for master cluster nodes"
+  description = "Private SSH key for master cluster nodes"
   value       = module.rke_rancher_master_cluster.ssh_private_key
   sensitive   = true
 }
 
 output "master_cluster_kubeconfig" {
-  description = "[SENSITIVE] KUBECONFIG yaml file contents to connect to the cluster. DO NOT USE unless you have no other options. Users should use the KUBECONFIG that Rancher provides to them rather than this."
+  description = "KUBECONFIG yaml file contents to connect to the cluster. DO NOT USE unless you have no other options. Users should use the KUBECONFIG that Rancher provides to them rather than this."
   value       = module.rke_rancher_master_cluster.cluster_kubeconfig
   sensitive   = true
 }
@@ -32,7 +32,7 @@ output "worker_ssh_public_key" {
 }
 
 output "worker_ssh_private_key" {
-  description = "[SENSITIVE] Private SSH key for the worker cluster nodes"
+  description = "Private SSH key for the worker cluster nodes"
   value       = module.rancher-k8s-cluster.ssh_private_key
   sensitive   = true
 }
